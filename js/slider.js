8ed31ca93a3ee8c64143a08e8fe9dd5d2cade6a0
DuetoPark <inhye.park.dev@gmail.com>
2022-04-27 17:33:29 +0900

    💄 Implement .today-best component

--- js/slider.js
+++ js/slider.js
@@ -14,3 +14,28 @@ const slider = tns({
   autoplayButtonOutput: false,
   controlsContainer: '.banner-carousel .slider-controls',
 });
+
+const todaySlider = tns({
+  container: '.today-best-carousel .slider-list',
+  nav: false,
+  items: 1,
+  gutter: 6,
+  edgePadding: 16,
+  preventScrollOnTouch: 'auto',
+  speed: 500,
+  mouseDrag: true,
+  controlsContainer: '.today-best-carousel .slider-controls',
+  loop: false,
+  responsive: {
+    768: {
+      items: 2,
+      edgePadding: 0,
+      mouseDrag: false,
+      slideBy: 2,
+    },
+    1170: {
+      items: 3,
+      slideBy: 3,
+    },
+  },
+});
